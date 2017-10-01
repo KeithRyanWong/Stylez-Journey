26750963dffb8bb5f2eafba523f0e8bcc5a18336
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-09-30 19:06:26 -0700

    Add style

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -1,14 +1,3 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-  let params = 
-  {
-    fontColor: "green",
-    backgroundColor: "black",
-    apn: "Stylez",
-    div: "#testbox",
-    nextBtn: 10,
-  };
 
-  MYAPPs.renderNavbar(params);
-
-  MYAPPs.showNextBtn();
 });
